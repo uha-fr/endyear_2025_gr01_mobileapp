d48a73d0ea0a904f06f8f805e0196e10c3239761
8idwan <ridouane.announ@gmail.com>
2025-05-09 17:57:47 +0200

    #17 création de la page clients

---
+++ lib/view/screen/clients.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClientsPage extends StatelessWidget {
+  const ClientsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Clients  Page");
+  }
+}
