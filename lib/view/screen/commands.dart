e8374499a66d81b2a827f7ee97956c8c0fcf05be
8idwan <ridouane.announ@gmail.com>
2025-05-09 17:56:55 +0200

    #10 création de la page commandes

---
+++ lib/view/screen/commands.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommandesPage extends StatelessWidget {
+  const CommandesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("commandes Page");
+  }
+}
