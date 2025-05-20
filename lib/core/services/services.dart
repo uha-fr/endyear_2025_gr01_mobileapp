5cdad5b53b70c0a8b4b1bb764162bc8f623a0a2c
Assil28 <assil.dkhil28@gmail.com>
2025-05-20 12:45:43 +0200

    fetchfromapi

--- lib/core/services/services.dart
+++ lib/core/services/services.dart
@@ -2,7 +2,7 @@ import 'dart:convert'; // Pour base64Encode et utf8
 import 'package:http/http.dart' as http;
 
 Future<void> fetchFromPythonApi() async {
-  var url = Uri.parse('http://10.0.2.2:5000/check_prestashop'); // 10.0.2.2 = localhost Android emulator
+  var url = Uri.parse('http://localhost/xampp/check_prestashop.php'); // 10.0.2.2 = localhost Android emulator
 
   try {
     final response = await http.get(url);
@@ -11,7 +11,10 @@ Future<void> fetchFromPythonApi() async {
       final data = jsonDecode(response.body);
       if (data['success']) {
         print('✔️ Réussi : ${data['message']}');
-        print('🔄 Données : ${data['data'].toString().substring(0, 200)}...');
+        final responseStr = data['response'].toString();
+final displayStr = responseStr.length > 200 ? responseStr.substring(0, 200) : responseStr;
+print('🔄 Données : $displayStr...');
+
       } else {
         print('❌ Échec : ${data['message']}');
       }
