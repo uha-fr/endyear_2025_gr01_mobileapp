8cd051a77293d39c120abf8f594ec60a1027dae9
Assil28 <assil.dkhil28@gmail.com>
2025-06-02 00:39:50 +0200

    fixation bug logout

--- lib/bindings/initbindings.dart
+++ lib/bindings/initbindings.dart
@@ -1,12 +1,15 @@
 import 'package:endyear_2025_gr01_mobileapp/core/class/crud.dart';
 import 'package:get/get.dart';
 import '../controller/configbackendcontroller.dart';
+import '../controller/auth/login_controller.dart';
 
 class InitialBindings extends Bindings {
   @override
   void dependencies() {
    Get.put(Crud());
    Get.put(ConfigBackendController());
+   // Register LoginControllerImp for global access
+   Get.lazyPut(() => LoginControllerImp(), fenix: true);
   }
   
 }
